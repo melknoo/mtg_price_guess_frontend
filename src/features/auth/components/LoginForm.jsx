92b285b50985b356d6da8c00df9e9843a966d493
melknoo <melvinknop@gmx.de>
2025-11-20 10:54:26 +0100

    test for captcha

--- src/features/auth/components/LoginForm.jsx
+++ src/features/auth/components/LoginForm.jsx
@@ -17,6 +17,8 @@ export default function LoginForm({ onForgotPassword }) {
   const [failedAttempts, setFailedAttempts] = useState(0);
   const [showCaptchaOnLogin, setShowCaptchaOnLogin] = useState(false);
 
+  console.log(process.env.REACT_APP_RECAPTCHA_SITE_KEY);
+
   // Prüfe beim Laden, ob bereits fehlgeschlagene Versuche gespeichert sind
   useEffect(() => {
     const attempts = parseInt(localStorage.getItem('login_attempts') || '0');
