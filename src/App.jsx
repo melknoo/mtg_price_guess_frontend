7949dee7009fa707593953fefb077ac88ec0012d
melknoo <melvinknop@gmx.de>
2025-11-20 14:51:27 +0100

    styling

--- src/App.jsx
+++ src/App.jsx
@@ -171,7 +171,7 @@ export default function App() {
 
   return (
     <div className="min-h-screen bg-gradient-to-br from-purple-900 to-indigo-900 text-white flex flex-col">
-      <div className="flex-1 flex flex-col items-center justify-center sm:p-4 py-12 px-4 relative">
+      <div className="flex-1 flex flex-col items-center justify-center sm:p-4 pt-12 pb-20 px-4 relative">
         <div className="absolute top-12 sm:top-4 right-4 flex gap-2">
           {screen === "game" && (
             <button
